9b12fdab69f3e07ad0ab16f01e0ba78c6479de89
justinanmoreno <justinanmoreno@gmail.com>
2021-03-22 12:57:50 -0400

    Project2 set-up

---
+++ config/database.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://Admin:idunno123@sei.fuetz.mongodb.net/whatscoming?retryWrites=true&w=majority', {
+	useNewUrlParser: true, 
+	useCreateIndex: true,
+	useUnifiedTopology: true 
+});
+
+// shortcut to mongoose.connection object
+const db = mongoose.connection;
+
+db.on('connected', function() {
+console.log(`Connected to MongoDB at ${db.host}:${db.port}`);
+});
