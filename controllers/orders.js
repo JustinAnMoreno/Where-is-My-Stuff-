9ee62ea6a87a88688a815a25491b08aa432c25b1
justinanmoreno <justinanmoreno@gmail.com>
2021-03-22 15:43:55 -0400

    draft

--- controllers/orders.js
+++ controllers/orders.js
@@ -1,10 +1,19 @@
-const { create } = require('../models/order');
 const Package = require('../models/order');
 
 
 function newOrder(req, res) {
  res.render('orders/new');
 }
+
+function create(req, res) { 
+    const order = newOrder(req.body);
+   
+    order.save(function(err) {
+      // for now, redirect right back to new.ejs
+      res.redirect('/orders/new');
+    });
+  }
+
 
 
 
