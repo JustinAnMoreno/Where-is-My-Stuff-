356ce843b424594c63e8a16587242e7a4078e853
justinanmoreno <justinanmoreno@gmail.com>
2021-03-22 21:11:10 -0400

    Schema Pre-Conditions

--- models/order.js
+++ models/order.js
@@ -1,18 +1,25 @@
 const mongoose = require('mongoose');
-// optional shortcut to the mongoose.Schema class
 const Schema = mongoose.Schema;
 
 //Schema defining individual order properties 
-const orderSchema = new Schema({
+const orderSchema = new Schema ({
+     orderSchema.pre('save', function (next) { 
+        // capitalize
+        this.carrier.charAt(0).toUpperCase() + this.carrier.slice(1);
+        next();
+      }),
     trackingNum : String,
-
-//Unsure if drop-down considered data-type
     // address : String,
     carrier : String,
-
     vendor: String,
     itemDescription : String,
 });
+
+// orderSchema.pre('save', function (next) {
+//     // capitalize
+//     this.carrier.charAt(0).toUpperCase() + this.carrier.slice(1);
+//     next();
+//   });
 
 //Compile schema into a model and export it 
 module.exports = mongoose.model('Orders', orderSchema);
