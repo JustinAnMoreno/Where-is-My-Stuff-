8fc602f9a964e0d9099464ab9c78f0d61b9d7883
justinanmoreno <justinanmoreno@gmail.com>
2021-03-25 10:19:53 -0400

    update function only works on manual refresh

--- controllers/orders.js
+++ controllers/orders.js
@@ -1,8 +1,20 @@
 const Order = require("../models/order");
 
-//renders "new" page
+//rendering all objects inside "Order" on orders/index page
+function index(req, res) {
+  Order.find({}, function (err, orders) {
+    res.render("orders/index", { title: "All Orders", orders });
+  });
+}
+//defining show funciton to specify 1 piece of object and display it's information
+function show(req, res) {
+  Order.findById(req.params.id, function (err, order) {
+    res.render("orders/show", { order, title: "Order Details" });
+  });
+}
+//renders "new order" page
 function newOrder(req, res) {
-  res.render("orders/new");
+  res.render("orders/new", { title: "Add Order" });
 }
 
 //defining "create" function/creating form and redirect route
@@ -12,39 +24,22 @@ function create(req, res) {
     res.redirect("/orders");
   });
 }
-
-//rendering all objects inside "Order" on orders/index page
-function index(req, res) {
-  Order.find({}, function (err, orders) {
-    res.render("orders/index", { orders });
+function edit(req, res) {
+  Order.findById(req.params.id, function (err, order) {
+    res.render("orders/edit", { order });
   });
 }
-
+function update(req, res) {
+  req.body.done = false
+  Order.findByIdAndUpdate(req.params.id, req.body, function (err, order) {
+    res.render("orders/show", { order });
+  });
+}
 //defining "delete" function. Finds specfic object by ID and deltes the data. Redirects to order page
 function deleteOrder(req, res) {
   Order.findByIdAndDelete(req.params.id).exec(function (err, order) {});
   res.redirect("/orders");
 }
-
-//defining show funciton to specify 1 piece of object and display it's information 
-function show(req, res) {
-  Order.findById(req.params.id, function(err, order){
-    res.render("orders/show", { order })
-}
-  )};
-
-function edit(req, res) {
-  Order.findById(req.params.id, function(err, order){
-    res.render("orders/edit", { order })
-  })
-}  
-
-function update(req, res) {
-  Order.findByIdAndUpdate(req.params.id, req.body, function(err, order){
-    res.render("orders/show", { order })
-  })
-}
-
 module.exports = {
   new: newOrder,
   create,
